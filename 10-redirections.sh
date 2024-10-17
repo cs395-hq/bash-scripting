2295d28ff78f41a12dc2fee9a362cdd11350d64d
Zafer Gurel <zafergurel@gmail.com>
2024-10-17 05:57:00 +0000

    fixes and more comments

--- 10-redirections.sh
+++ 10-redirections.sh
@@ -2,61 +2,84 @@
 
 # Redirections
 # ============
-# Redirecting standart output and standard error
-ls &>ls.log
-# Same as
-ls >ls.log 2>&1
+# Redirecting standart outpuot and standard error
+# 0, 1, and 2 are the file descriptor versions 
+# of stdin, stdout, and stderr, respectively.
 
-# Append to a file
-ls >>ls.log 2>&1
-# Same as (does not work on MacOS)
-# ls -l &>>ls.log
+ls &>ls.log
+echo "The content of ls.log"
+cat ls.log
+rm ls.log
+# Same as the following
+# ls 1>ls.log 2>&1
+# ls  >ls.log 2>&1
+#
+# But NOT same as with the following
+# The following duplicates file descriptor 1 to f.d. 2
+# Then, the file descriptor of the ls.log file is duplicated as 1
+# So, the stuff that goes to 2 now actually goes to stdout (printed out)
+# The stuff that goes to 1 goes to ls.log.
+# ls 2>&1 >ls.log 
+#
+
+# Copying descriptos
+# ------------------
+# Let's copy descriptors' original values.
+# >& means redirection but it duplicates the file descriptor 1 to 3.
+# By doing so, both file descriptors point to the same device.
+# In our case it's stdout.
+# So that everything that sent to f.d. 3 goes stdout. 
+# Same for f.d. 4. It now points to stderr.
+exec 3>&1
+exec 4>&2
+exec 1>output.log # Notice that we don't use & for files.
+exec 2>&1
+echo "this is going to FD 1 -> output.log" >&1
+echo "this is going to FD 2 -> output.log" >&2 
+echo "Write to FD 3 -> stdout. This is displayed in the terminal." >&3
+echo "Write to FD 4 -> stderr. This is also displayed in the terminal." >&4 
+# restore 1 and 3
+exec 1>&3
+exec 2>&4
+echo "The content of output log." 
+cat output.log
+rm output.log
+echo
+
+# close 3 and 4
+exec 3>&-
+exec 4>&-
+
+# Append to a file.
+echo "This creates or overwrites the file." > append.log
+echo "This is appended to the file by redirecting FD 1." >> append.log
+echo "This also appends but redirecting both FDs 1 and 2." >>append.log 2>&1
+# Same as (does NOT work on MacOS)
+# echo "" &>>append.log
+
+echo "The content of append.log"
+cat append.log
+rm append.log
+echo
 
 # Redirects the standard output to /dev/null
 # /dev/null is a special file that discards all data written to it.
 # Copies the standard error to the standard output.
-ffmpeg > /dev/null 2>&1
-
-# copies the standard error to the standard output and
-# redirects the standard output to /dev/null.
-#ffmpeg 2>&1 > /dev/null
-
-# Redirecting to a file
-# Order matters
-#ffmpeg 2>&1 > ffmpeg.log
-#ffmpeg > ffmpeg.log 2>&1
+echo "This is not displayed in the terminal." > /dev/null 2>&1
 
 # Here strings
 echo -n "Number of characters in the here string: "
 wc -c <<< "here string character count!"
-
+echo
 # Here documents
-# The delimiter must be at the beginning of the line.
+# The marker (here, it's EOF but can be another string) 
+# must be at the beginning of the line. The marker
+# indicates the end of the text.
 echo -n "Number of lines in the here document:"
 wc -l<<EOF
 Here document to the file!
 Number of lines.
 It's three.
 EOF
-
-# Copying file descriptors
-echo "Some text" > tmp.log
-exec 3< tmp.log
-cat <&3
-
-# Close the file descriptor
-exec 3<&-
-rm tmp.log
-
-# Copying stdout to another file descriptor
-exec 3>&1
-exec 1>tmp.log
-echo "This will be written to tmp.log"
-# Get back to the original stdout
-exec 1>&3
-echo "This will be written to the original stdout"
-echo "Check the tmp.log file"
-cat tmp.log
-rm tmp.log
-
+echo
 
