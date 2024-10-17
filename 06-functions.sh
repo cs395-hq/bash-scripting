16c72164b17c91526f97ff9cacab33f00dc364eb
Zafer Gurel <zafergurel@gmail.com>
2024-10-17 04:46:36 +0000

    refactoring

--- 06-functions.sh
+++ 06-functions.sh
@@ -3,26 +3,28 @@
 # The variable var is defined in three different scopes:
 # 1. Global scope (outside of any function)
 # 2. Local scope (inside func1)
+# Run the script passing at least one argument. 
+# ./06-functions.sh my-script-arg
 
 func1()
 {
-    local var='func1 local'
+    local var='func1 local variable'
     func2
 }
 
 func2()
 {
-    echo "In func2, var = $var"
+    echo "In func2, var = $var <- func1 scope is effective"
 }
 
 func3()
 {
-	echo $0
+	echo '$0 in func3 is' $0 # This does not change
 	echo "In func3, positional parameter 1: $1"
 }
 
-var="global"
+var="global variable"
 func1
 echo "After func1, var = $var"
-func3 "hello"
+func3 "Argument passed to func3"
 echo "Positional parameter 1 outside func3: $1"
